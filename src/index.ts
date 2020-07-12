b5ff9e0735bdbd11f4da78e401da401fd6d4790d
PH <1836071395@qq.com>
2020-07-12 15:05:17 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { useState, useMemo, useRef, Reducer, SetStateAction, ReducerState, ReducerAction, Dispatch, useEffect } from 'react'
+import { useState, useMemo, useRef, Reducer, SetStateAction, ReducerState, ReducerAction, Dispatch, useEffect, useCallback } from 'react'
 import {  TeleStateInterface, TeleState, UpdatePlugin } from './tele-state'
 
 let stateId = 0
@@ -15,10 +15,34 @@ const useTele = <R extends Reducer<any, any>>(
 }
 
 
+// type Reducers<R extends Reducer<any,any>> = {[actionName: string]: R}
+// type ReducersReducer<RS extends Reducers<any>> =   RS extends Reducers<infer R>? R : never
+type ReducersReducer<S> = <P>(s: S, paload: P) => S
+type Reducers<S> = { [actionName: string]: ReducersReducer<S> }
+type ReducersState<RS extends Reducers<any>> = RS extends  Reducers<infer S>? S: never
+type ReducersPalyLoad<RS extends Reducers<any>, T extends keyof RS> = 
+ RS[T] extends ReducersReducer<ReducersState<RS>, infer P>? P :
+ never
+type ReducersAction<RS extends Reducers<any>, T extends keyof RS> = {type: T, payLoad:  ReducersPalyLoad<RS, T>}
+
+export const createTeleReducers = <RS extends Reducers<any>>(reducers: RS, initState: ReducersState<RS> ) =>{
+
+  const reducer = <T extends keyof RS>(preState:ReducersState<RS>, action: ReducersAction<RS, T>  ) => {
+    
+    return reducers[action.type](preState, action.payLoad)
+  }
+  return createTeleReducer<Reducer<
+  ReducersState<RS>,
+  ReducersAction<RS, keyof RS>
+  >>(reducer, initState)
+}
+
 const createTeleReducer = <R extends Reducer<any, any>>(
   reducer: R,
   initState: ReducerState<R>, 
 ) => {
+  
+ 
   const teleState: TeleStateInterface<ReducerState<R>, ReducerAction<R>> = 
   new TeleState<ReducerState<R>, ReducerAction<R>>(initState, reducer)
   return {
@@ -29,14 +53,7 @@ const createTeleReducer = <R extends Reducer<any, any>>(
   }
 }
 
-
-const stateReducer = <S>(preState: S, action: SetStateAction<S>) => {
-  if(typeof action === 'function'){
-    return (action as (preState: S) => S )(preState)
-  }else{
-    return action
-  }
-} 
+const stateReducer = <S>(preState: S, action: SetStateAction<S>) => typeof action === 'function'? (action as (preState: S) => S )(preState) : action
 
 const createTeleState = <S>(initialState: SetStateAction<S>) => {
   const { useTeleReducer, ...rest } = createTeleReducer<Reducer<S, SetStateAction<S> >>( stateReducer, stateReducer(null, initialState ))
@@ -46,4 +63,19 @@ const createTeleState = <S>(initialState: SetStateAction<S>) => {
   }
 }
 
+
+
+
 export { createTeleReducer, createTeleState }
+
+interface RReducers extends Reducers<number> {
+  add: (s:number, p: number) => number
+  // remove:(s:number) => number
+}
+const reducers: RReducers = {
+  add: (s:number, p: number) => s + a,
+
+}
+const { useTeleReducer }= createTeleReducers(reducers, 1)
+const [ a, d ] = useTeleReducer()
+d({ type: 'add', payLoad: })
