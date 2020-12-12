03d6e0256ddb2f299b4229851752b66766d853e7
PH <1836071395@qq.com>
2020-12-12 20:05:48 +0800

    gl base shap shader

--- stories/0-Welcome.stories.tsx
+++ stories/0-Welcome.stories.tsx
@@ -73,44 +73,71 @@ uniform vec2 u_windowSize;
 
 attribute vec2 a_position;
 attribute vec4 a_color;
-attribute float a_r;
+attribute vec2 a_size;
+attribute float a_type; // 1:circle; 2: rectangle;
 
-varying vec4 v_color;
-varying float v_r;
-varying vec2 v_position;
 varying vec2 v_windowSize;
+varying vec4 v_color;
+varying vec2 v_size;
+varying vec2 v_position;
+varying float v_type;
 
 void main(){
   v_color = a_color;
-  gl_Position = vec4((a_position/u_windowSize *2.0 -1.0) * vec2(1, -1), 0, 1 );
-  v_position = a_position;
-
-  v_r = a_r;
-  gl_PointSize = 2.0* a_r;
-
   v_windowSize = u_windowSize;
+  v_position = a_position;
+  v_size = a_size;
+  v_type = a_type;
+
+  gl_Position = vec4((a_position/u_windowSize *2.0 -1.0) * vec2(1, -1), 0, 1 );
+  
+  if (a_type >0.0 && a_type <= 1.0) {
+    // circle;
+    gl_PointSize = 2.0 * a_size.x;
+    return;
+  }
+  if (a_type>1.0 && a_type <= 2.0) {
+    // rectangle
+    gl_PointSize = max(a_size.x, a_size.y );
+    return;
+  }
+  // gl_PointSize = a_size.x;
 }
 `
 
 const fgShaderStr = `
 precision highp float;
 
-varying vec4 v_color;
-varying float v_r;
-varying vec2 v_position;
 varying vec2 v_windowSize;
+varying vec4 v_color;
+varying vec2 v_size;
+varying vec2 v_position;
+varying float v_type;
 
-// gl_FragCoord
-// gl_PointCoord
+
+
 void main(){
 
-  float dist = distance( v_position, vec2(gl_FragCoord.x, v_windowSize.y - gl_FragCoord.y)  );
-  if(dist> v_r) {
-    discard;
-  }else{
-    // * smoothstep( v_r, v_r-0.0001,  dist)
-    gl_FragColor = vec4(v_color.x, v_color.y, v_color.z, v_color.w );    
+  if (v_type >0.0 && v_type <= 1.0) {
+    // circle;
+    float dist = distance( ceil(v_position), ceil(vec2(gl_FragCoord.x, v_windowSize.y - gl_FragCoord.y))  );
+    if(ceil(dist)> ceil(v_size.x)) {
+      discard;
+    }else{
+      gl_FragColor = vec4 (v_color.x, v_color.y, v_color.z, smoothstep( v_size.x, v_size.x-2.0, dist));  
+      // gl_FragColor = v_color;  
+    }
+    // gl_FragColor = v_color;
+    return;
   }
+
+  if (v_type>1.0 && v_type <= 2.0) {
+    // rectangle
+    gl_FragColor = v_color;
+    return;
+  }
+ 
+  
 }
 `
 
@@ -118,52 +145,75 @@ void main(){
 
 const positions = [
   // 200, 200,
-  // 300,300,
-  // 600,300,
-
-  // 100,100,
-  // 200,200,
-  // 100,200,
+  // 200 -100, 200 -100,
+  // 200 +100, 200 - 100,
+  // 200,200+100,
 ];
-const c = [ 0,0,0,1]
-const colors = [
-  // 1,0,0,1,
-  // 0,1,0,1,
-  // 0,0,1,1,
-
-  // ...c,
-  // ...c,
-  // ...c,
+const shapColor = [
+  // 1 *255 ,0 *255, 0 *255,1 *255,
+  // 0 *255 ,1 *255, 0 *255,1 *255,
+  // 0 *255 ,0 *255, 1 *255,1 *255,
+  // 1 *255 ,1 *255, 0 *255,1 *255,
 ]
-const r = [
-  // 100,
-  // 10,
-  // 50,
-  
-  // 10,
-  // 100,
-  // 50,
+const shapSize = [
+  // 200, 0,
+  // 50, 0,
+  // 50, 0,
+  // 80, 0,
+
 ]
+const shapeType = [
+  // 1,
+  // 1,
+  // 1,
+  // 2,
+]
+const canvasWidth = 1400
+const canvasHeight = 1400
+const wCount = 100
+const hCount = 100
 
-const wCount = 500
-const hCount = 200
-
-const pad = 0.1
-const w = (1400/ wCount)-pad
-const h = (800/ hCount) - pad
+const pad = 2
+const w = (canvasWidth/ wCount)-pad
+const h = (canvasHeight/ hCount) - pad
 for(let i =0 ; i< wCount; i++){
   for(let j = 0; j< hCount; j++ ){
-    const start = {x: (w + pad) * i + 0.5 *w , y: (h + pad)* j}
+    const start = {x: (w + pad) * i + 0.5 *w , y: (h + pad)* j + 0.5 *w }
     // const angle = [
     //  start.x, start.y,
     //  start.x, start.y+h,
     //  start.x + w, start.y+h  
     // ]
+    const r = w * 0.5
     positions.push(start.x, start.y)
-    colors.push(1,0,0,1)
-    r.push(w * 0.5)
+    shapColor.push(0.45,0.75,0.2,1)
+    shapSize.push(r, 0)
+    shapeType.push(1)
+
+    const halfR = 0.5 * r
+    positions.push(start.x -halfR , start.y)
+    shapColor.push(0.25,0.45,0.2,1)
+    shapSize.push(halfR, 0)
+    shapeType.push(1)
+
+    positions.push(start.x + halfR , start.y)
+    shapColor.push(0.25,0.45,0.2,1)
+    shapSize.push(halfR, 0)
+    shapeType.push(1)
+
+    positions.push(start.x, start.y + r)
+    shapColor.push(0.8,0.15,0.2,1)
+    shapSize.push(halfR, 0)
+    shapeType.push(2)
+
+
   }
 }
+
+console.log('total unit', wCount * hCount * 4, 'count: ', wCount * hCount)
+
+
+// const positionArray = new Float32Array(positions)
 
 
 
@@ -193,98 +243,106 @@ const Test = () => {
     gl.attachShader(program, fragShader)
     gl.linkProgram(program)
     console.log('link program', gl.getProgramParameter(program, gl.LINK_STATUS))
-
-
-    const positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
-    const positionArray = new Float32Array(positions)
-    gl.bufferData(gl.ARRAY_BUFFER, positionArray, gl.STATIC_DRAW);
-    const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-    gl.enableVertexAttribArray(positionAttributeLocation);
-    // 告诉属性怎么从positionBuffer中读取数据 (ARRAY_BUFFER)
-    let size = 2;          // 每次迭代运行提取两个单位数据
-    let type = gl.FLOAT;   // 每个单位的数据类型是32位浮点型
-    let normalize = false; // 不需要归一化数据
-    let stride = 0;        // 0 = 移动单位数量 * 每个单位占用内存（sizeof(type)）
-                          // 每次迭代运行运动多少内存到下一个数据开始点
-    let offset = 0;        // 从缓冲起始位置开始读取
-    gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset)
-
-    const colorBuffer = gl.createBuffer()
-    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer)
-    const colorArray = new Uint8Array(colors)
-    gl.bufferData(gl.ARRAY_BUFFER, colorArray, gl.STATIC_DRAW);
-    const colorAttributeLocation = gl.getAttribLocation(program, "a_color");
-    gl.enableVertexAttribArray(colorAttributeLocation);
-    // 告诉属性怎么从positionBuffer中读取数据 (ARRAY_BUFFER)
-    size = 4;          // 每次迭代运行提取两个单位数据
-    type = gl.UNSIGNED_BYTE;   // 每个单位的数据类型是32位浮点型
-    normalize = false; // 不需要归一化数据
-    stride = 0;        // 0 = 移动单位数量 * 每个单位占用内存（sizeof(type)）
-                          // 每次迭代运行运动多少内存到下一个数据开始点
-    offset = 0;        // 从缓冲起始位置开始读取
-    gl.vertexAttribPointer(colorAttributeLocation, size, type, normalize, stride, offset)
-
-
-    const raduisBuffer = gl.createBuffer()
-    gl.bindBuffer(gl.ARRAY_BUFFER, raduisBuffer)
-    const raduisArray = new Float32Array(r)
-    gl.bufferData(gl.ARRAY_BUFFER, raduisArray, gl.STATIC_DRAW);
-    const raduisAttributeLocation = gl.getAttribLocation(program, "a_r");
-    gl.enableVertexAttribArray(raduisAttributeLocation);
-    // 告诉属性怎么从positionBuffer中读取数据 (ARRAY_BUFFER)
-    size = 1;          // 每次迭代运行提取两个单位数据
-    type = gl.FLOAT;   // 每个单位的数据类型是32位浮点型
-    normalize = false; // 不需要归一化数据
-    stride = 0;        // 0 = 移动单位数量 * 每个单位占用内存（sizeof(type)）
-                          // 每次迭代运行运动多少内存到下一个数据开始点
-    offset = 0;        // 从缓冲起始位置开始读取
-    gl.vertexAttribPointer(raduisAttributeLocation, size, type, normalize, stride, offset)
+    const shaderAttribuites = {
+      a_position: gl.getAttribLocation(program, 'a_position'),
+      a_color: gl.getAttribLocation(program, 'a_color'), 
+      a_size: gl.getAttribLocation(program, 'a_size'), 
+      a_type: gl.getAttribLocation(program, 'a_type'), 
+    }
 
     
+
+
+
+    
+    gl.useProgram(program)
+
+    const viewSize = gl.getUniformLocation(program, 'u_windowSize')
+    gl.uniform2f(viewSize, gl.canvas.width, gl.canvas.height)
+
 
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
 
     gl.clearColor(0, 0, 0, 0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    
-    gl.useProgram(program)
-
-    
-
-    const viewSize = gl.getUniformLocation(program, 'u_windowSize')
-    gl.uniform2f(viewSize, gl.canvas.width, gl.canvas.height)
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
     gl.enable(gl.BLEND)
     gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
 
-    
+    const positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+    const positionArray = new Float32Array(positions)
+    gl.bufferData(gl.ARRAY_BUFFER, positionArray, gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(shaderAttribuites.a_position);
+    // 告诉属性怎么从positionBuffer中读取数据 (ARRAY_BUFFER)
+    // let size = 2;          // 每次迭代运行提取两个单位数据
+    // let type = gl.FLOAT;   // 每个单位的数据类型是32位浮点型
+    // let normalize = false; // 不需要归一化数据
+    // let stride = 0;        // 0 = 移动单位数量 * 每个单位占用内存（sizeof(type)）
+    //                       // 每次迭代运行运动多少内存到下一个数据开始点
+    // let offset = 0;        // 从缓冲起始位置开始读取
+    gl.vertexAttribPointer(shaderAttribuites.a_position, 2, gl.FLOAT, false, 0, 0)
+
+    const shapeTypeBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, shapeTypeBuffer)
+    const shapeTypeArray = new Float32Array(shapeType)
+    gl.bufferData(gl.ARRAY_BUFFER, shapeTypeArray, gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(shaderAttribuites.a_type);
+    // 告诉属性怎么从positionBuffer中读取数据 (ARRAY_BUFFER)
+    // size = 1;          // 每次迭代运行提取两个单位数据
+    // type = gl.FLOAT;   // 每个单位的数据类型是32位浮点型
+    // normalize = false; // 不需要归一化数据
+    // stride = 0;        // 0 = 移动单位数量 * 每个单位占用内存（sizeof(type)）
+    //                       // 每次迭代运行运动多少内存到下一个数据开始点
+    // offset = 0;        // 从缓冲起始位置开始读取
+    gl.vertexAttribPointer(shaderAttribuites.a_type, 1, gl.FLOAT, false, 0, 0)
+
+    const colorBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer)
+    const colorArray = new Float32Array(shapColor)
+    gl.bufferData(gl.ARRAY_BUFFER, colorArray, gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(shaderAttribuites.a_color);
+    // 告诉属性怎么从positionBuffer中读取数据 (ARRAY_BUFFER)
+    // size = 4;          // 每次迭代运行提取两个单位数据
+    // type = gl.FLOAT;   // 每个单位的数据类型是32位浮点型
+    // normalize = true; // 不需要归一化数据
+    // stride = 0;        // 0 = 移动单位数量 * 每个单位占用内存（sizeof(type)）
+    //                       // 每次迭代运行运动多少内存到下一个数据开始点
+    // offset = 0;        // 从缓冲起始位置开始读取
+    gl.vertexAttribPointer(shaderAttribuites.a_color, 4, gl.FLOAT, false, 0, 0)
 
 
-  
-    // gl.drawArrays(primitiveType, offset, count);
-    // buffer[0] = 1
-    // buffer[1] = 1
-    // gl.bufferData(gl.ARRAY_BUFFER, buffer, gl.STATIC_DRAW);
-    // gl.drawArrays(primitiveType, offset, count);
+    const sizeBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, sizeBuffer)
+    const sizeArray = new Float32Array(shapSize)
+    gl.bufferData(gl.ARRAY_BUFFER, sizeArray, gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(shaderAttribuites.a_size);
+    // 告诉属性怎么从positionBuffer中读取数据 (ARRAY_BUFFER)
+    // size = 2;          // 每次迭代运行提取两个单位数据
+    // type = gl.FLOAT;   // 每个单位的数据类型是32位浮点型
+    // normalize = false; // 不需要归一化数据
+    // stride = 0;        // 0 = 移动单位数量 * 每个单位占用内存（sizeof(type)）
+    //                       // 每次迭代运行运动多少内存到下一个数据开始点
+    // offset = 0;        // 从缓冲起始位置开始读取
+    gl.vertexAttribPointer(shaderAttribuites.a_size, 2, gl.FLOAT, false, 0, 0)
 
-    const primitiveType = gl.POINTS;
-    var count =positions.length/2;
+
     const draw = () => {
-      positionArray.forEach((_, ind) =>   positionArray[ind] += 0.1 )
+      positionArray.forEach((_, ind) =>  {
+        positionArray[ind] += 0.1
+      })
+      gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
       gl.bufferData(gl.ARRAY_BUFFER, positionArray, gl.STATIC_DRAW);
-      gl.drawArrays(primitiveType, offset, count);
-      // requestAnimationFrame(draw)
+
+      gl.drawArrays(gl.POINTS, 0, positions.length * 0.5);
+      requestAnimationFrame(draw)
     }
     draw()
 
   }, [])
 
   return <div style={{ backgroundColor: 'black' }} >
-   <canvas ref={cRef} width={1400} height={800} ></canvas>
+   <canvas ref={cRef} width={canvasWidth} height={canvasHeight} style={{ width : canvasWidth / devicePixelRatio, height: canvasHeight/devicePixelRatio}} ></canvas>
   </ div >
 }
 
